86e3fa807dd1ccf780e57f0b8d14f5c201ebff64
Charlie Machado <charlie.machado@stern.nyu.edu>
2023-08-14 14:43:02 -0400

    Updated Testing for trranscription

--- Test/youtube_dload_test.py
+++ Test/youtube_dload_test.py
@@ -1,24 +1,33 @@
-##TEST WILL ASK YOU TO INPUT A YOUTUBE URL, WE SUGGEST A SHORT ONE DUE TO RUNTIM ##
-## https://www.youtube.com/watch?v=VsuShNWghXk&ab_channel=wikiHow IS A GOOD URL TO USE## 
+
 
 import pytest
 from app.alpha import API_KEY
 from app.youtube_dload import download_and_transcribe
+from unittest import mock
 
 def test_download_and_transcribe(monkeypatch, capsys):
-    # Simulate user input for the YouTube URL
-    monkeypatch.setattr('builtins.input', lambda _: "https://www.youtube.com/watch?v=VsuShNWghXk&ab_channel=wikiHow")
+    # Mock the API_KEY and API request to avoid actual API calls
+    with mock.patch('app.alpha.API_KEY', 'your_mocked_api_key'), \
+         mock.patch('openai.Audio.transcribe') as mock_transcribe:
 
-    # Call the function with the simulated user input
-    download_and_transcribe()
+        # Set the mock return value for the transcript
+        mock_transcribe.return_value = "Mocked transcript text"
 
-    # Capture the printed transcript
-    captured = capsys.readouterr()
-    transcript = captured.out.strip()
+        # Simulate user input for the YouTube URL
+        monkeypatch.setattr('builtins.input', lambda _: "https://www.youtube.com/watch?v=VsuShNWghXk&ab_channel=wikiHow")
 
-    # Add assertions to validate the transcript
-    assert isinstance(transcript, str)
-    assert len(transcript) > 0
+        # Call the function with the simulated user input
+        download_and_transcribe()
+
+        # Capture the printed transcript
+        captured = capsys.readouterr()
+        transcript = captured.out.strip()
+
+        # Assertions
+        assert isinstance(transcript, str)
+        assert len(transcript) > 0
+
+
 
 
 
--- app/youtube_dload.py
+++ app/youtube_dload.py
@@ -1,32 +1,22 @@
 from pytube import YouTube
-
 import openai
-
 from app.alpha import API_KEY
-
 
 def download_and_transcribe(video_url=None):
     if video_url is None:
         video_url = input("Please input your YouTube URL: ")
 
     # Downloading the video using PYTUBE
-    #video_url = input("Please input your youtube URL: ")
     yt = YouTube(video_url)
     yt.streams.filter(only_audio=True)
     stream = yt.streams.get_by_itag(22)
     stream.download()
 
-
-
-
     # create variable for the downloaded file
     downloaded_filename = stream.download()
 
-
-
-    ## WHISPER App from Open AI to CREATE TRANSCRIPT
-
-    openai.api_key = API_KEY  # supply your API key however you choose
+    # WHISPER App from Open AI to CREATE TRANSCRIPT
+    openai.api_key = API_KEY
     f = open(downloaded_filename, "rb")
     transcript = openai.Audio.transcribe("whisper-1", f)
 
