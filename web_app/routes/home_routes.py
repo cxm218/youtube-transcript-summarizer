28e461e16e57ec3ea30ff4cbca29deb079fcca47
Charlie Machado <charlie.machado@stern.nyu.edu>
2023-08-13 20:15:53 -0400

    Downloads video - Removed Stock and Unemployment tabs
    Still struggling to get it to properly redirect to another page when it pulls the youtube video

--- web_app/routes/home_routes.py
+++ web_app/routes/home_routes.py
@@ -1,29 +1,28 @@
 from flask import Blueprint, request, render_template, redirect, url_for
+from pytube import YouTube
 
 home_routes = Blueprint("home_routes", __name__)
 
 @home_routes.route("/")
 @home_routes.route("/home")
 def index():
-    print("HOME...")
     return render_template('home.html')
-    #return render_template("home.html")
 
 @home_routes.route("/pull_audio", methods=["POST"])
 def pull_audio():
     if request.method == "POST":
         video_url = request.form.get("video_url")
-        # Download the audio here
-        # Redirect to the Summarize page with the video URL as a query parameter
-        return redirect(url_for("home_routes.summarize", video_url=video_url))
+
+        # Downloading the video using PYTUBE
+        yt = YouTube(video_url)
+        yt.streams.filter(only_audio=True)
+        stream = yt.streams.get_by_itag(22)
+        stream.download()
+        # Redirect to the Summarize page
+        return redirect(url_for("home_routes.summarize"))
     
-    return render_template('home.html')
+    return render_template('pull_audio.html')
 
 @home_routes.route("/summarize")
 def summarize():
-    print("Summarize Video...")
-    video_url = request.args.get("video_url")
-    # Perform summarization logic here with the video URL
-    summarized_text = "This is a placeholder for the summarized text."
-
-    return render_template("summarize_result.html", summarized_text=summarized_text)
+    return render_template("summarize_input.html")
